60ce9b0914d6772828ba646576084ec7cb4ea71b
ChiaraSolito <chiara.solito00@gmail.com>
2022-05-07 12:29:19 +0200

    init

---
+++ src/MainApp.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("HELLO!");
+    }
+}
