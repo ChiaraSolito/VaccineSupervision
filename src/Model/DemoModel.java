7e19db8426d7039ee04b6b12a588c95136730d8d
ChiaraSolito <chiara.solito00@gmail.com>
2022-05-09 16:04:24 +0200

    partially deleting DemoModel in favor of User

--- src/Model/DemoModel.java
+++ src/Model/DemoModel.java
@@ -4,76 +4,30 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.BooleanPropertyBase;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.scene.control.Alert;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class DemoModel {
 
-    private StringProperty user = new SimpleStringProperty("");
+    /*
+        Metodi per l'accesso al database
+     */
+    private void manageSQLException(Exception e){
+        e.printStackTrace();
+        System.err.println(e.getClass().getName()+": "+e.getMessage());
+        System.exit(0);
+    }
 
-    private StringProperty password = new SimpleStringProperty("");
-
-    private BooleanProperty Medico = new BooleanPropertyBase() {
-        @Override
-        public Object getBean() {
-            return null;
+    private ResultSet runQuery(String q){
+        try{
+            ResultSet r = null;
+            //Statement stmt = c.createStatement();
+            //r = stmt.executeQuery(q);
+            return r;
+        } catch (Exception e) {
+            manageSQLException(e);
         }
-
-        @Override
-        public String getName() {
-            return null;
-        }
-    };
-
-    private BooleanProperty Farmacologo = new BooleanPropertyBase() {
-        @Override
-        public Object getBean() {
-            return null;
-        }
-
-        @Override
-        public String getName() {
-            return null;
-        }
-    };
-
-
-    public BooleanProperty medicoProperty(){
-        return Medico;
-    }
-
-    public BooleanProperty farmacologoProperty(){
-        return Farmacologo;
-    }
-
-    public void setFarmacologo(boolean b){
-        Farmacologo.set(b);
-    }
-
-    public void setMedico(boolean b){
-        Medico.set(b);
-    }
-
-    public boolean getFarmacologo(){
-        return Farmacologo.get();
-    }
-
-    public boolean getMedico(){
-        return Medico.get();
-    }
-
-    public String getUser() {
-        return user.get();
-    }
-
-    public StringProperty userProperty() {
-        return user;
-    }
-
-    public String getPassword() {
-        return password.get();
-    }
-
-    public StringProperty passwordProperty() {
-        return password;
+        return null;
     }
 }
