8bbc86ece714fb83167241c2ced18f70f30f9ea9
VirginiaFilippi <virginia.filippi@studenti.univr.it>
2022-05-12 10:25:39 +0200

    modified hello

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setScene(new Scene(new LoginController(primaryStage).getView()));
-        primaryStage.setTitle("Login Page");
+        primaryStage.setTitle("Ciao Page");
         primaryStage.show();
     }
 }
