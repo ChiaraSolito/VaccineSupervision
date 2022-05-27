d7ef34d0d4f702cd49326510799581ebd6e5d458
VirginiaFilippi <virginia.filippi@studenti.univr.it>
2022-05-27 10:23:31 +0200

    added comments for explanation

--- src/Model/Utils/DAO/ReportDAO.java
+++ src/Model/Utils/DAO/ReportDAO.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 public interface ReportDAO {
     List<Report> getAllReports(String username);
+    //metodo usato solo per farmacologo
+    //username serve per fare controllo che sia farmacologo
     void createReport(String id, String idPatient, String reactionName,
                       Date reportDate, Date reactionDate,
                       String vaccination, SimpleStringProperty doctor);
+    //id viene creato dentro tramite una query serial + 1 (altrimenti il seriale non è più seriale)
 }
