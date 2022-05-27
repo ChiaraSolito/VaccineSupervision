b4708f08cd2155ce8a3d4a2dc912dcd35d0f7b67
ChiaraSolito <chiara.solito00@gmail.com>
2022-05-27 11:15:23 +0200

    added implementation of Vaccination DAO

--- src/Model/Utils/DAO/VaccinationDAO.java
+++ src/Model/Utils/DAO/VaccinationDAO.java
@@ -3,13 +3,14 @@ package Model.Utils.DAO;
 import Model.Report;
 import Model.Vaccination;
 
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
 public interface VaccinationDAO {
     void createVaccination(String idPatient, String vaccine,
                            String typeSomministration, String vaccinationSite,
-                           Date vaccinationDate);
-    List<Vaccination> getAllVaccination(String idPatient);
-    List<Vaccination> getTwoMonthsVaccination(String idPatient);
+                           Date vaccinationDate) throws SQLException;
+    List<Vaccination> getAllVaccination(String idPatient) throws SQLException;
+    List<Vaccination> getTwoMonthsVaccination(String idPatient) throws SQLException;
 }
