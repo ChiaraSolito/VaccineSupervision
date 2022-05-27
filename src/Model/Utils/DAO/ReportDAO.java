18d7149df66054df20799f2c1c132021336ed283
ChiaraSolito <chiara.solito00@gmail.com>
2022-05-27 11:56:19 +0200

    added implementation of Report DAO

--- src/Model/Utils/DAO/ReportDAO.java
+++ src/Model/Utils/DAO/ReportDAO.java
@@ -2,15 +2,17 @@ package Model.Utils.DAO;
 
 import Model.Report;
 import javafx.beans.property.SimpleStringProperty;
+
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
 public interface ReportDAO {
-    List<Report> getAllReports(String username);
+    List<Report> getAllReports(String username) throws SQLException;
     //metodo usato solo per farmacologo
     //username serve per fare controllo che sia farmacologo
     void createReport(String id, String idPatient, String reactionName,
                       Date reportDate, Date reactionDate,
-                      String vaccination, SimpleStringProperty doctor);
+                      String vaccination, SimpleStringProperty doctor) throws SQLException;
     //id viene creato dentro tramite una query serial + 1 (altrimenti il seriale non è più seriale)
 }
---
+++ src/Model/Utils/DAOImpl/ReportDAOImpl.java
@@ -0,0 +1,75 @@
+package Model.Utils.DAOImpl;
+
+import Model.ControlPhase;
+import Model.DataBase.DataBaseConnection;
+import Model.Report;
+import Model.Utils.DAO.ReportDAO;
+import Model.Vaccination;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ReportDAOImpl implements ReportDAO {
+    DataBaseConnection pConnection;
+
+    @Override
+    public List<Report> getAllReports(String username) throws SQLException {
+        pConnection = new DataBaseConnection();
+        pConnection.openConnection();
+
+        List<Report> reports = new ArrayList<>();
+
+        pConnection.statement = pConnection.connection.createStatement();
+        pConnection.rs = pConnection.statement.executeQuery("SELECT R.id, R.reportdate, R.reactiondate, R.reaction, R.idpatient, R.doctor " +
+                "FROM report R ");
+
+        while (pConnection.rs.next()) {
+            List<Vaccination> listaVuota = new ArrayList<>();
+            reports.add(new Report(
+                    new SimpleStringProperty(pConnection.rs.getString("R.id")),
+                    new SimpleObjectProperty(pConnection.rs.getString("R.idpatient")),
+                    new SimpleObjectProperty(pConnection.rs.getString("R.reaction")),
+                    new SimpleStringProperty(pConnection.rs.getString("R.reportdate")),
+                    new SimpleStringProperty(pConnection.rs.getString("R.reactiondate")),
+                    listaVuota,
+                    new SimpleStringProperty(pConnection.rs.getString("R.doctor"))
+            ));
+        }
+
+        for (Report r : reports) {
+            pConnection.rs = pConnection.statement.executeQuery("SELECT V.idpatient, V.vaccine, V.typesomministration, V.vaccinationsite, V.vaccinationdate " +
+                    "FROM Vaccination V JOIN Report R ON R.idpatient = V.idpatient " +
+                    "WHERE R.idpatient = '" + r.getPatient() + "' AND V.vaccinationdate BETWEEN R.reactiondate - 60 AND R.reactiondate");
+
+            while (pConnection.rs.next()) {
+                r.addVaccination(new Vaccination(
+                        new SimpleObjectProperty(pConnection.rs.getString("V.idpatient")),
+                        new SimpleStringProperty(pConnection.rs.getString("V.vaccine")),
+                        new SimpleStringProperty(pConnection.rs.getString("V.typesomministration")),
+                        new SimpleStringProperty(pConnection.rs.getString("V.vaccinationsite")),
+                        new SimpleStringProperty(pConnection.rs.getString("V.vaccinationdate"))
+                ));
+            }
+        }
+
+        pConnection.closeConnection();
+        return reports;
+    }
+
+    @Override
+    public void createReport(String id, String idPatient, String reactionName, Date reportDate, Date reactionDate, String vaccination, SimpleStringProperty doctor) throws SQLException {
+        pConnection = new DataBaseConnection();
+        pConnection.openConnection();
+
+        pConnection.statement = pConnection.connection.createStatement();
+        pConnection.rs = pConnection.statement.executeQuery("INSERT INTO report " +
+                "VALUES('" + id + "', CURRENT_DATE, '" + reactionDate + "', '" + reactionName + "', '" + idPatient + "', '" + doctor + "')"
+        );
+
+        pConnection.closeConnection();
+    }
+}
