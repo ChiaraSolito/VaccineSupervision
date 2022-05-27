93e907922e4e2b14a2dcf7f8e661ef0f9abc21eb
VirginiaFilippi <virginia.filippi@studenti.univr.it>
2022-05-27 08:47:19 +0200

    changed createRiskFactor in RiskFactorDAO, changed return in getPatient in PatientDAOImpl

--- src/Model/Utils/DAO/RiskFactorDAO.java
+++ src/Model/Utils/DAO/RiskFactorDAO.java
@@ -1,9 +1,11 @@
 package Model.Utils.DAO;
 
 import Model.RiskFactor;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public interface RiskFactorDAO {
-    List<RiskFactor> getAllRiskFactors(String idPatient);
-    void createRiskFactor(String description, int riskLevel);
+    List<RiskFactor> getAllRiskFactors(String idPatient) throws SQLException;
+    void createRiskFactor(String name, String description, int riskLevel) throws SQLException;
 }
