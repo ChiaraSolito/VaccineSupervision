fb1e68125d3dc2ed675a47dddda45df58700aed0
VirginiaFilippi <virginia.filippi@studenti.univr.it>
2022-06-24 09:40:44 +0200

    fixed warnings

--- src/Test/PharmacologistControllerTest/ReportAnalysisControllerTest.java
+++ src/Test/PharmacologistControllerTest/ReportAnalysisControllerTest.java
@@ -1,8 +1,6 @@
 package Test.PharmacologistControllerTest;
 
 import Control.FarmacologistControl.ReportAnalysisController;
-import Control.FarmacologistControl.ReportListController;
-import Model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
